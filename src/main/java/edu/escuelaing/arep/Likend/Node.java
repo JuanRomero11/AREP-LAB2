d7a95c6ecfb3d7c58f7699b9e2ac9441512cd4f0
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-02-04 01:24:26 -0500

    despliegue y barra funcional

---
+++ src/main/java/edu/escuelaing/arep/Likend/Node.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arep.Likend;
+
+/**
+ *
+ * @author Juan Romero
+ */
+class Node<T> {
+    private T val= null;
+    private Node next= null;
+    
+    public Node(T e){
+        this.val = e;
+    }
+    
+    public void setNext(Node n){
+        this.next=n;
+    }
+    public T getT(){
+        return val;
+    }
+    public void setT(T nuevo){
+       this.val=nuevo ;
+    }
+    public Node getNext(){
+        return next;
+    }
+}
--- src/main/java/edu/escuelaing/arep/sparkweb/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/sparkweb/SparkWebApp.java
@@ -4,16 +4,26 @@ package edu.escuelaing.arep.sparkweb;
  * @Autor Juan Romero
  *
  */
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.port;
 
-import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.escuelaing.arep.Likend.LikendList;
+import spark.ModelAndView;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
 public class SparkWebApp {
    
     public static void main(String[] args) {
+    	LikendList<Double> lista1 = new LikendList<Double>();
         port(getPort());
-        get("/hello", (req, res) -> "hello heroku");   
-
+        get("/hello", (req, res) -> "<input/><button>ingrese lista de numeros</button>"); 
+       
     }
+  
     static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
